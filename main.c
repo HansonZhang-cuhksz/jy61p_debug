e03a546943119d54ed8282686143a6d36519f4c8
HansonZhang-cuhksz <123090823@link.cuhk.edu.cn>
2025-01-10 17:26:49 +0800

    read 10

--- main.c
+++ main.c
@@ -45,38 +45,38 @@ int main(int argc,char* argv[]){
 	while (1)
 	{
 		// WitReadReg(AX, 12);
-		WitReadReg(AX, 1);
+		WitReadReg(AX, 10);
 		usleep(500000);
-		// if(s_cDataUpdate)
-		// {
-		// 	printf("Entering...\r\n");
-		// 	for(i = 0; i < 3; i++)
-		// 	{
-		// 		fAcc[i] = sReg[AX+i] / 32768.0f * 16.0f;
-		// 		fGyro[i] = sReg[GX+i] / 32768.0f * 2000.0f;
-		// 		fAngle[i] = sReg[Roll+i] / 32768.0f * 180.0f;
-		// 	}
-		// 	if(s_cDataUpdate & ACC_UPDATE)
-		// 	{
-		// 		printf("acc:%.3f %.3f %.3f\r\n", fAcc[0], fAcc[1], fAcc[2]);
-		// 		s_cDataUpdate &= ~ACC_UPDATE;
-		// 	}
-		// 	if(s_cDataUpdate & GYRO_UPDATE)
-		// 	{
-		// 		printf("gyro:%.3f %.3f %.3f\r\n", fGyro[0], fGyro[1], fGyro[2]);
-		// 		s_cDataUpdate &= ~GYRO_UPDATE;
-		// 	}
-		// 	if(s_cDataUpdate & ANGLE_UPDATE)
-		// 	{
-		// 		printf("angle:%.3f %.3f %.3f\r\n", fAngle[0], fAngle[1], fAngle[2]);
-		// 		s_cDataUpdate &= ~ANGLE_UPDATE;
-		// 	}
-		// 	if(s_cDataUpdate & MAG_UPDATE)
-		// 	{
-		// 		printf("mag:%d %d %d\r\n", sReg[HX], sReg[HY], sReg[HZ]);
-		// 		s_cDataUpdate &= ~MAG_UPDATE;
-		// 	}
-		// }
+		if(s_cDataUpdate)
+		{
+			printf("Entering...\r\n");
+			for(i = 0; i < 3; i++)
+			{
+				fAcc[i] = sReg[AX+i] / 32768.0f * 16.0f;
+				fGyro[i] = sReg[GX+i] / 32768.0f * 2000.0f;
+				fAngle[i] = sReg[Roll+i] / 32768.0f * 180.0f;
+			}
+			if(s_cDataUpdate & ACC_UPDATE)
+			{
+				printf("acc:%.3f %.3f %.3f\r\n", fAcc[0], fAcc[1], fAcc[2]);
+				s_cDataUpdate &= ~ACC_UPDATE;
+			}
+			if(s_cDataUpdate & GYRO_UPDATE)
+			{
+				printf("gyro:%.3f %.3f %.3f\r\n", fGyro[0], fGyro[1], fGyro[2]);
+				s_cDataUpdate &= ~GYRO_UPDATE;
+			}
+			if(s_cDataUpdate & ANGLE_UPDATE)
+			{
+				printf("angle:%.3f %.3f %.3f\r\n", fAngle[0], fAngle[1], fAngle[2]);
+				s_cDataUpdate &= ~ANGLE_UPDATE;
+			}
+			if(s_cDataUpdate & MAG_UPDATE)
+			{
+				printf("mag:%d %d %d\r\n", sReg[HX], sReg[HY], sReg[HZ]);
+				s_cDataUpdate &= ~MAG_UPDATE;
+			}
+		}
 	}
 	
 	
