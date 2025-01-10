ce89fb6a1276a6e99b9ed35f7d3bf7359b3e49fa
HansonZhang-cuhksz <123090823@link.cuhk.edu.cn>
2025-01-10 17:07:39 +0800

    lines

--- wit_c_sdk.c
+++ wit_c_sdk.c
@@ -377,25 +377,26 @@ int32_t WitReadReg(uint32_t uiReg, uint32_t uiReadNum)
            break;
         case WIT_PROTOCOL_I2C:
 		      if(p_WitI2cReadFunc == NULL){
-                    printf("p_WitI2cReadFunc is NULL");
                     return WIT_HAL_EMPTY;
                 }
               usTemp = uiReadNum << 1;
+              printf("usTemp = %d\n", usTemp);
               if(WIT_DATA_BUFF_SIZE < usTemp){
-                    printf("WIT_DATA_BUFF_SIZE < usTemp");
                     return WIT_HAL_NOMEM;
                 }
               if(p_WitI2cReadFunc(s_ucAddr << 1, uiReg, s_ucWitDataBuff, usTemp) == 1)
               {
+                printf("p_WitI2cReadFunc passed");
                   if(p_WitRegUpdateCbFunc == NULL){
-                        printf("p_WitRegUpdateCbFunc is NULL");
                         return WIT_HAL_EMPTY;
                   }
                   for(i = 0; i < uiReadNum; i++)
                   {
                       sReg[i+uiReg] = ((uint16_t)s_ucWitDataBuff[(i<<1)+1] << 8) | s_ucWitDataBuff[i<<1];
                   }
+                  printf("for loop passed");
                   p_WitRegUpdateCbFunc(uiReg, uiReadNum);
+                  printf("p_WitRegUpdateCbFunc passed");
               }
 			break;
 		default: 
